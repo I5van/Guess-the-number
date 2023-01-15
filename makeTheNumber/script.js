461d2aa26b656e5153c1b8cac4703b01d3b085ff
I5van <n5im@yandex.ru>
2023-01-15 17:46:56 +0300

    22233

--- makeTheNumber/script.js
+++ makeTheNumber/script.js
@@ -1,9 +1,63 @@
-let button=document.querySelector(".popup__button")
+let button = document.querySelector(".popup__button")
 let popup = document.querySelector(".popup")
-function showPopup(){
-    popup.style.display ="flex"
+let timerId
+let lessButton = document.querySelector("#less")
+let moreButton = document.querySelector("#more")
+let rightNumberButton = document.querySelector("#true")
+let right = 100
+let left = 0
+function less() {
+    if(timerId != null){
+        return
+    }
+    right = Math.floor((left + right) / 2)
+    animateNumbers()
+}
+function more() {
+    if(timerId != null){
+        return
+    }
+    left = Math.floor((left + right) / 2)
+    animateNumbers()
+ 
 
 }
-button.addEventListener("click",showPopup)
+function rightNumber() {
+    counter.innerHTML = "Ура!!"
+}
+
+let counter = document.querySelector(".counter")
+function RandomNumber(start, end) {
+    let randomInt = start + Math.random() * (end - start + 1)
+    return Math.floor(randomInt)
+
+
+
+}
+function timer() {
+    counter.innerHTML = RandomNumber(1, 100)
+
+}
+function animateNumbers() {
+    timerId = setInterval(timer, 100)
+    let timerEnd = setTimeout(() => {
+        clearInterval(timerId)
+        timerId=null
+        counter.innerHTML = Math.floor((left + right) / 2)
+    }, 2000)
+}
+function showPopup() {
+    popup.style.display = "flex"
+    animateNumbers()
+
+}
+function closePopup() {
+    popup.style.display = "none"
+    clearInterval(timerId)
+}
+button.addEventListener("click", showPopup)
 let closed = document.querySelector(".close")
-closed.onclick.popup.style.display="none"
+closed.addEventListener("click", closePopup)
+lessButton.addEventListener("click", less)
+moreButton.addEventListener("click", more)
+rightNumberButton.addEventListener("click", rightNumber)
